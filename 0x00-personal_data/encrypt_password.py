449ba335b7012d4cd20639825cb5ad98dfaee2e6
Krizdaniels <dannychris2@gmail.com>
2024-07-04 19:39:59 +0000

    Personal Data

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,10 +1,11 @@
-# encrypt_password.py
+#!/usr/bin/env python3
+""" Returns a salted, hashed password, byte in string """
 import bcrypt
 
-def hash_password(password: str) -> str:
-    """Hashes a password using bcrypt."""
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+def hash_password(password: str) -> bytes:
+    """ Returns byte string password """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
-def is_valid(hashed_password: str, password: str) -> bool:
-    """Validates a password against a given hash."""
-    return bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8'))
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Implement is_valid to validate provided password matched hashed_password """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
