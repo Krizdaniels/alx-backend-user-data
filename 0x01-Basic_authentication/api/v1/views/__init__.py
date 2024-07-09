50d7c6510413d1dc687023a8978176aaa2d28594
Krizdaniels <dannychris2@gmail.com>
2024-07-10 00:19:15 +0100

    basic

--- 0x01-Basic_authentication/api/v1/views/__init__.py
+++ 0x01-Basic_authentication/api/v1/views/__init__.py
@@ -8,9 +8,6 @@ app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 from api.v1.views.index import *
 from api.v1.views.users import *
 
-<<<<<<< HEAD
-User.load_from_file()
-=======
 # Ensure the User class is correctly imported from models.user
 from models.user import User
 
