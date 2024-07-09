1c2fdabfa5ecbbdce79f55c07cab819185b4a881
Krizdaniels <dannychris2@gmail.com>
2024-07-09 23:03:49 +0100

    basic

--- 0x01-Basic_authentication/api/v1/views/__init__.py
+++ 0x01-Basic_authentication/api/v1/views/__init__.py
@@ -2,12 +2,13 @@
 """ DocDocDocDocDocDoc
 """
 from flask import Blueprint
-from api.v1.views.app_views import app_views
-
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *
 from api.v1.views.users import *
+
+# Ensure the User class is correctly imported from models.user
+from models.user import User
 
 User.load_from_file()
